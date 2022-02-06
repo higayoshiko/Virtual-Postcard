b4d0f8cbd8a01dc2826fd426336b92dd1b80b871
ejaman <leegm042@gmail.com>
2022-02-07 01:14:45 +0900

    hide search function

--- virtualPostcard/src/components/grid/Grid.jsx
+++ virtualPostcard/src/components/grid/Grid.jsx
@@ -1,32 +1,38 @@
 import React, { useState } from "react";
 import styles from "./Grid.module.css";
 import DisplayImage from "../displayImage/DisplayImage";
-import Mail from "../makeMail/Mail";
+import Mail from "../Mail/Mail";
 import ImgApi from "../imgApi/imgApi";
 
 const Grid = (props) => {
   const [image, setImage] = useState("");
-
+  const [open, setOpen] = useState(false);
   const onClickImg = (event) => {
     setImage(event.target.src);
+  };
+  const onClickOpen = (event) => {
+    setOpen((prevStatus) => (prevStatus ? false : true));
   };
 
   return (
     <section className={styles.container}>
       <div className={styles.top}>
-        <div className={styles.displayImg}>
+        <div onClick={onClickOpen} className={styles.displayImg}>
           <DisplayImage imageTarget={image} />
         </div>
+
         <div className={styles.email}>
           <Mail sendtoMail={image} />
         </div>
       </div>
 
-      <div className={styles.bottom}>
-        <div className={styles.img}>
-          <ImgApi onClickImg={onClickImg} />
+      {open ? (
+        <div className={styles.bottom}>
+          <div className={styles.img}>
+            <ImgApi onClickImg={onClickImg} />
+          </div>
         </div>
-      </div>
+      ) : null}
     </section>
   );
 };
